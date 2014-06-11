b72a7cd011861686cc35f3e6bef7fca292c8472d
Daniel Rampanelli <hello@neuralquery.com>
2014-06-11 20:09:14 +0200

    Use the THEME_SLUG placeholder for the slug version of the theme name for file paths.

--- samples/compass/production.rb
+++ samples/compass/production.rb
@@ -1,6 +1,6 @@
 # require 'module'
 
-http_path = "/wp-content/themes/%THEME_NAME%/"
+http_path = "/wp-content/themes/%THEME_SLUG%/"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
